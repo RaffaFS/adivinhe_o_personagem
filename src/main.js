0422d254376f136fc15614abd23b5c6a6fe9eed8
Rafael Gonçalves Ribeiro <raffafullstack@gmail.com>
2024-05-29 13:12:35 -0300

    criando lógica de captação e analise de resposta do usuário

--- src/main.js
+++ src/main.js
@@ -1,42 +1,69 @@
-const chars = ['Naruto', 'Vegeta']
+// A lógica inicial está salva e explicada em outro arquivo, vamos aqui fazer
+// esse código ser executado através do front-end, basicamente vou apenas
+// fazer algum uso do DOM para colocar toda a lógica dentro de uma função
+// e trabalhar com eventos (além disso mudei a function star lá pra baixo)
+
+const chars = ['NARUTO', 'VEGETA']
+let nome = ''
 let tips = []
 
-// Criação dos arrays para personagens e dicas
-// Para personagens uma const por ser fixa e para dicas uma let (e vazia), para
-// ser redefinida na chamada de cada personagem
+const btn1 = document.querySelector('#btn1')
+btn1.addEventListener('click', start)
 
-const randomN = Math.floor(Math.random() * chars.length)
+function start(){
+    const randomN = Math.floor(Math.random() * chars.length)
 
-function randomTip(){
-    const randomT = Math.floor(Math.random() * tips.length)
-    console.log(tips[randomT])
+    switch(randomN){
+        case 0:
+            nome = 'NARUTO'
+            tips = [
+                'O mais famoso portador das nove caudas',
+                '"Esse é o meu jeito ninja de ser"',
+                'Usuário do Rasengan'
+            ]        
+            randomTip()
+            break
+    
+        case 1:
+            nome = 'VEGETA'
+            tips = [
+                'Maior rival do Goku',
+                'Príncipe dos Sayajins',
+                'Marido da Bulma'
+            ]        
+            randomTip()
+            break
+    }
+
+    function randomTip(){
+        const randomT = Math.floor(Math.random() * tips.length)
+        const resBox = document.querySelector('#results_box')
+        resBox.innerHTML = `<p>${tips[randomT]}</p>`
+
+// Aqui começamos o desenvolvimento da segunda etapa que, no caso,
+// diz respeito à captação e análise da resposta do usuário
+
+        const form = document.querySelector('#form')
+        form.addEventListener('submit', analisar)
+
+        function analisar(e){
+            e.preventDefault()
+            const inputV = document.querySelector('#userInput')
+            const inputUp = inputV.value.toUpperCase()
+
+            if(inputUp == nome){
+                resBox.innerHTML = 'acertou'
+            }
+            else{
+                resBox.innerHTML = 'errou'
+            }
+        }
+        
+        const inputV = document.querySelector('#userInput')
+        inputV.value = ''
+    }
 }
 
-// Geração de um número pseudoaleatório com base no comprimento do array acima
-// Esse número será usado pelo Switch-case mais abaixo. Além disso temos
-// também a criação da função que será chamada dentro de cada caso quando em
-// execução. Ela gera um número aleatório que corresponderá à um índice de "tips"
-// que atualmente tem valor nulo, mas a cada chamada, terá seu valor sobreescrito
-// para o personagem correspondente
-
-
-switch(randomN){
-    case 0:
-        tips = [
-            'O mais famoso portador das nove caudas',
-            '"Esse é o meu jeito ninja de ser"',
-            'Usuário do Rasengan'
-        ]        
-        randomTip()
-        break
-
-    case 1:
-        tips = [
-            'Maior rival do Goku',
-            'Príncipe dos Sayajins',
-            'Marido da Bulma'
-        ]        
-        randomTip()
-        break
-}
-
+// Ainda há de ser melhorada, mas já está funcional, a próxima parte
+// será o desenvolvimento do armazenamento de pontos e depois dessa
+// um sistema de rodadas máximas e retorno visual dos pontos obtidos
